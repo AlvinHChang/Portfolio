4909a7dae5c988c5c3e80a956dddb5af9d60787b
AlvinHChang <solstory@gmail.com>
2019-01-24 15:36:55 -0600

    Updates to design

---
+++ app/components/Dropdown/Dropdown.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+import styles from './Dropdown.css';
+
+// requires react transition group and css modules
+
+class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggled: false,
+    };
+  }
+
+  toggleDropdown = () => {
+    if (this.state.toggled) {
+      document.removeEventListener('click', this.handleClickOutside);
+    } else {
+      document.addEventListener('click', this.handleClickOutside);
+    }
+    this.setState(prevState => ({ toggled: !prevState.toggled }));
+  };
+
+  handleClickOutside = event => {
+    if (!this.node.contains(event.target)) {
+      this.toggleDropdown();
+    }
+  };
+
+  handleMouseEnter = () => {
+    if (this.props.isHover) {
+      this.setState({ toggled: true });
+    }
+  };
+
+  handleMouseLeave = () => {
+    if (this.props.isHover) {
+      this.setState({ toggled: false });
+    }
+  };
+
+  handleClick = () => {
+    if (!this.props.isHover) {
+      this.toggleDropdown();
+    }
+  };
+
+  render() {
+    const { customButton, dropdownItem, dropdownBackgroundColor } = this.props;
+    return (
+      <div
+        ref={node => {
+          this.node = node;
+        }}
+        className={styles.dropdown}
+      >
+        <div
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+        >
+          <button
+            className={styles.dropdownButton}
+            type="button"
+            onClick={this.handleClick}
+          >
+            {customButton}
+          </button>
+          <CSSTransitionGroup
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}
+            transitionName={{
+              enter: styles['example-enter'],
+              enterActive: styles['example-enter-active'],
+              leave: styles['example-leave'],
+              leaveActive: styles['example-leave-active'],
+            }}
+          >
+            {this.state.toggled ? (
+              <div
+                style={{ backgroundColor: dropdownBackgroundColor }}
+                className={styles.dropdownItem}
+              >
+                {dropdownItem}
+              </div>
+            ) : null}
+          </CSSTransitionGroup>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dropdown.propTypes = {
+  customButton: PropTypes.node,
+  dropdownItem: PropTypes.object,
+  dropdownBackgroundColor: PropTypes.string,
+  isHover: PropTypes.bool,
+};
+
+export default Dropdown;
