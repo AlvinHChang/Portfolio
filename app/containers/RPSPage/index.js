3f65a6a1ef31386d29f3c61bcf885943957a4475
AlvinHChang <solstory@gmail.com>
2019-01-25 16:34:42 -0600

    Initial Rock Paper Scissor Game

---
+++ app/containers/RPSPage/index.js
@@ -0,0 +1,38 @@
+/*
+ * FeaturePage
+ *
+ * List all the features
+ */
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+
+import H1 from 'components/H1';
+import RockPaperScissor from 'components/RockPaperScissor/RockPaperScissor';
+import messages from './messages';
+
+export default class RPSPage extends React.Component {
+  // Since state and props are static,
+  // there's no need to re-render this component
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>RPS Demo Page</title>
+          <meta
+            name="description"
+            content="Feature page of React.js Boilerplate application"
+          />
+        </Helmet>
+        <H1>
+          <FormattedMessage {...messages.header} />
+        </H1>
+        <RockPaperScissor />
+      </div>
+    );
+  }
+}
